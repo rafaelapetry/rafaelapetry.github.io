89c6a53e3a6dd1152cd3f9803b6ac9a8b5cae141
rafaelapetry <103939248+rafaelapetry@users.noreply.github.com>
2022-10-10 08:36:35 -0300

    Update scripts.js

--- tudo/projetos/projeto_assync/scripts.js
+++ tudo/projetos/projeto_assync/scripts.js
@@ -3,7 +3,7 @@ const max = 11; // número da última imagem
 const updateRate = 2000;
 
 function proxImagem (img){
-    fetch("img/"+img+".jpg")
+    fetch("img/"+img+".png")
         .then(resp => resp.blob())
         .then(blob => {
             const imageObjectURL = URL.createObjectURL(blob); // cria endereço da imagem
